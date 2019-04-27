7a207523b959d446573b0f03e3f6fd8c99f6e83c
yuki-tashiro <>
2019-04-27 15:57:03 +0900

    Add Element-ui

---
+++ src/views/MainList.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="main-list">
+    <el-button @click="searchApi">API通信</el-button>
+    <el-row>
+      <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
+        <el-card :body-style="{ padding: '0px' }">
+          <img
+            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            class="image"
+          >
+          <div style="padding: 14px;">
+            <span>Yummy hamburger</span>
+            <div class="bottom clearfix">
+              <time class="time">{{ currentDate }}</time>
+              <el-button type="text" class="button">Operating</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mainList",
+  components: {},
+  data() {
+    return {
+      currentDate: new Date(),
+      results: null
+    };
+  },
+  methods: {
+    searchApi() {
+      axios
+        .get("http://127.0.0.1:8000/api/v1/infomation/?format=json")
+        .then(response => (this.results = response.data));
+    }
+  }
+};
+</script>
+
+<style>
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
+</style>
