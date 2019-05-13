0298c2d9dbe668623650c90d86aa3ac9540d6c0d
yuki-tashiro <>
2019-05-13 20:06:16 +0900

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
     <el-menu class="menu-bar" :default-active="activeIndex" mode="horizontal" router=true>
-      <el-menu-item index="/">Home
+      <el-menu-item index="/">main-List
         <!-- <router-link to="/">Home</router-link> -->
       </el-menu-item>
       <el-menu-item index="/about">about
         <!-- <router-link to="about">About</router-link> -->
       </el-menu-item>
-      <el-menu-item index="/main-list">main-list
+      <el-menu-item index="/home">Home
         <!-- <router-link to="main-list">MainList</router-link> -->
       </el-menu-item>
       <el-menu-item index="/test">test
