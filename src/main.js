3470dc87852a679705e3348f1cdbc0939a81d3f7
yuki-tashiro <>
2019-06-03 20:22:01 +0900

    add：npmアップデート、pwaプラグイン追加

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import './plugins/axios';
 import App from './App.vue';
 import router from './router';
+
 // Element-ui の読み込み、useの宣言
 import './plugins/element.js';
+
+import './registerServiceWorker'
 
 Vue.config.productionTip = false; // 製品情報の表示切替
 Vue.config.devtools = true // 本番・開発切り替え(chromeのVue devtoolの使用切り替え)
